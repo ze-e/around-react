803cb31c963671a614d519bc922c53bb7fa12021
ze-e <white.noise616@gmail.com>
2020-09-29 16:57:50 -0700

    feat: hide delete button if not owned

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,6 +14,11 @@ function Card(props){
   const cardDeleteButtonClassName = (
     `element__delete-button ${isOwn && 'element__delete-button_visibility_visible'}`
   ); 
+
+    //check if card was already liked
+  const isLiked = card.likes.some(i => i._id === currentUser._id);
+  const cardLikeButtonClassName = `element__like-button ${isLiked && 'element__like-button_stateliked'}`; 
+
   return(
   <>
   <div className={cardDeleteButtonClassName}></div>
@@ -21,7 +26,7 @@ function Card(props){
   <div className="element__text">
     <h2 className="element__title">{props.card.name}</h2>
     <div className="element__likes-container">
-      <button className="element__like-button"></button>
+      <button className={cardLikeButtonClassName}></button>
       <p className="element__likes-display">{props.card.likes.length}</p>
     </div>
   </div>
