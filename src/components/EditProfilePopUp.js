c305f8aa728cf26bc012d3432b142a45c200c94d
ze-e <white.noise616@gmail.com>
2020-10-02 13:27:15 -0700

    refactor:added initial values to EditProfilePopup

--- src/components/EditProfilePopUp.js
+++ src/components/EditProfilePopUp.js
@@ -6,8 +6,13 @@ function EditProfilePopUp(props){
   // Subscription to the context
   const currentUser = React.useContext(CurrentUserContext);
 
-  const [name, setName] = React.useState('');
-  const [description, setDescription] = React.useState('');
+  const [name, setName] = React.useState(currentUser.name);
+  const [description, setDescription] = React.useState(currentUser.about);
+
+  React.useEffect(()=>{
+    setName(currentUser.name)
+    setDescription(currentUser.about)
+  },[currentUser])
 
   function handleSubmit(e) {
     //check if the form is valid before sending
@@ -65,9 +70,9 @@ function handleChange(e) {
       <form className={`popup__form popup__form_type_edit-profile`} onSubmit={handleSubmit} onChange={validateForm} ref={formRef}>
         <button className={`popup__close popup__close_type_edit-profile`} type="button" onClick={props.onClose}></button>  
         <h4 className="popup__title">Edit Profile</h4>
-        <input className={`popup__input popup__input-name ${nameError !==null && 'popup__input_type_error'}`} id="name-input" type="text" name="name" placeholder={currentUser.name} required minLength="2" maxLength="40" value={name} onChange={handleChange}/>
+        <input className={`popup__input popup__input-name ${nameError !==null && 'popup__input_type_error'}`} id="name-input" type="text" name="name" required minLength="2" maxLength="40" value={name} onChange={handleChange}/>
           <span className={`popup__input-error" id="name-input-error ${nameError !==null && 'popup__error_visible'}`}>{nameError}</span>
-        <input className={`popup__input popup__input-description ${descriptionError !==null && 'popup__input_type_error'}`} id="description-input" type="text" name="description" placeholder={currentUser.about} required minLength="2" maxLength="200" value={description} onChange={handleChange}/>
+        <input className={`popup__input popup__input-description ${descriptionError !==null && 'popup__input_type_error'}`} id="description-input" type="text" name="description" required minLength="2" maxLength="200" value={description} onChange={handleChange}/>
           <span className={`popup__input-error" id="description-input-error ${descriptionError !==null && 'popup__error_visible'}`}>{descriptionError}</span>
         <button className={`popup__submit popup__edit-profile-submit ${formInvalid && 'popup__submit_disabled'}`} disabled={formInvalid} type="submit" >Save</button>
       </form>
