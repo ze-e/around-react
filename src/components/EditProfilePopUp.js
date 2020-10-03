5ba7c903072f5accf0f61cc28b0fd3c51e37ff48
ze-e <white.noise616@gmail.com>
2020-10-02 20:02:46 -0700

    refactor:added fieldValidator common method

--- src/components/EditProfilePopUp.js
+++ src/components/EditProfilePopUp.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {CurrentUserContext} from '../contexts/CurrentUserContext';
-import formValidator from '../utils/formvalidator'
+import {formValidator, fieldValidator} from '../utils/formvalidator'
 
 function EditProfilePopUp(props){
   
@@ -35,21 +35,28 @@ function EditProfilePopUp(props){
     and form/button validation controls the ability to send the form
   */
   const formRef = React.useRef();
-  const [nameError, setNameError] = React.useState(null);
-  const [descriptionError, setDescriptionError] = React.useState(null);
+  const [nameError, setNameError] = React.useState('');
+  const [descriptionError, setDescriptionError] = React.useState('');
   const [formInvalid, setFormInvalid] = React.useState(true)
 
 function handleChange(e) {
-  e.target.name === 'name' && setName(e.target.value);
-  e.target.name === 'description'&& setDescription(e.target.value);
-
-  //validate fields and display any errors
-  inputValidation(e.target);
+  if(e.target.name === 'name'){
+    //set value
+    setName(e.target.value);
+    //set error
+    fieldValidator(e.target, setNameError)
+  }
+  if(e.target.name === 'description'){
+    //set value
+    setDescription(e.target.value);
+    //set error
+    fieldValidator(e.target, setDescriptionError)
+  }
 }
 
   function inputValidation(input){
-    (input.name === 'name' && !input.validity.valid) ? setNameError(input.validationMessage) : setNameError(null);
-    (input.name === 'description' && !input.validity.valid) ? setDescriptionError(input.validationMessage) : setDescriptionError(null); 
+    (input.name === 'name' && !input.validity.valid) ? setNameError(input.validationMessage) : setNameError('');
+    (input.name === 'description' && !input.validity.valid) ? setDescriptionError(input.validationMessage) : setDescriptionError(''); 
   }
 
 
@@ -63,10 +70,10 @@ function handleChange(e) {
       <form className={`popup__form popup__form_type_edit-profile`} onSubmit={handleSubmit} onChange={validateForm} ref={formRef}>
         <button className={`popup__close popup__close_type_edit-profile`} type="button" onClick={props.onClose}></button>  
         <h4 className="popup__title">Edit Profile</h4>
-        <input className={`popup__input popup__input-name ${nameError !==null && 'popup__input_type_error'}`} id="name-input" type="text" name="name" required minLength="2" maxLength="40" value={name} onChange={handleChange}/>
-          <span className={`popup__input-error" id="name-input-error ${nameError !==null && 'popup__error_visible'}`}>{nameError}</span>
-        <input className={`popup__input popup__input-description ${descriptionError !==null && 'popup__input_type_error'}`} id="description-input" type="text" name="description" required minLength="2" maxLength="200" value={description} onChange={handleChange}/>
-          <span className={`popup__input-error" id="description-input-error ${descriptionError !==null && 'popup__error_visible'}`}>{descriptionError}</span>
+        <input className={`popup__input popup__input-name ${nameError !=='' && 'popup__input_type_error'}`} id="name-input" type="text" name="name" required minLength="2" maxLength="40" value={name} onChange={handleChange}/>
+          <span className={`popup__input-error" id="name-input-error ${nameError !=='' && 'popup__error_visible'}`}>{nameError}</span>
+        <input className={`popup__input popup__input-description ${descriptionError !=='' && 'popup__input_type_error'}`} id="description-input" type="text" name="description" required minLength="2" maxLength="200" value={description} onChange={handleChange}/>
+          <span className={`popup__input-error" id="description-input-error ${descriptionError !=='' && 'popup__error_visible'}`}>{descriptionError}</span>
         <button className={`popup__submit popup__edit-profile-submit ${formInvalid && 'popup__submit_disabled'}`} disabled={formInvalid} type="submit" >Save</button>
       </form>
     </div>
--- src/utils/formvalidator.js
+++ src/utils/formvalidator.js
@@ -1,7 +1,10 @@
-export default function formValidator(form, selector){
-  console.log(form, selector);
+export function formValidator(form, selector){
   const inputList = Array.from(form.querySelectorAll(selector));
   return inputList.some((inputElement) => {
     return !inputElement.validity.valid;
   }) ? true : false;
+}
+
+export function fieldValidator(input, errorFunction){
+  !input.validity.valid ? errorFunction(input.validationMessage) : errorFunction('');
 }
